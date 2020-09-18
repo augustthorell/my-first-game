2ff12e5c4225f890f4eed1cdcabdf26e2dfde674
August Thorell <augustthorell@gmail.com>
2020-09-18 16:30:39 +0200

    Added thumbs and some animations

--- Rock-paper-scissors/app.js
+++ Rock-paper-scissors/app.js
@@ -1,17 +1,45 @@
 // Generate a random number and choosing rock, paper, scissor for the computer //
-function random() {
+function random(a) {
     let randomNumber = Math.floor(Math.random() * 3);
     let computer = document.getElementById('computer');
+    let result = document.getElementById('show-result');
+    let rock = 0;
+    let paper = 1;
 
-    if (randomNumber == 0) {
+    if (randomNumber == rock) {
         computer.innerHTML = '<img src=images/rock.svg>';
-
-    } else if (randomNumber == 1) {
-        computer.innerHTML = '<img src=images/scissor.svg>';
-    } else {
+        if (randomNumber == a) {
+            result.innerHTML = "It's a Draw";
+        } else if (a == 2) {
+            result.innerHTML = "You Lose";
+            document.querySelector('.thumb').style.display = "block";
+        } else if (a == 1) {
+            result.innerHTML = "You Win";
+            document.querySelector('.thumb').style.display = "block";
+        }
+    } else if (randomNumber == paper) {
         computer.innerHTML = '<img src=images/paper.svg>';
+        if (randomNumber == a) {
+            result.innerHTML = "It's a Draw";
+        } else if (a == 0) {
+            result.innerHTML = "You Lose";
+            document.querySelector('.thumb').style.display = "block";
+        } else if (a == 2) {
+            result.innerHTML = "You Win";
+            document.querySelector('.thumb').style.display = "block";
+        }
+    } else {
+        computer.innerHTML = '<img src=images/scissor.svg>';
+        if (randomNumber == a) {
+            result.innerHTML = "It's a Draw";
+        } else if (a == 1) {
+            result.innerHTML = "You Lose";
+            document.querySelector('.thumb').style.display = "block";
+        } else if (a == 0) {
+            result.innerHTML = "You Win";
+            document.querySelector('.thumb').style.display = "block";
+        }
     }
-    
 }
 
 
@@ -19,16 +47,17 @@ function random() {
 
 function rock() {
     document.getElementById('userInput').innerHTML = '<img src=images/rock.svg>';
-    random();
+    random(0);
 }
 function paper() {
     document.getElementById('userInput').innerHTML = '<img src=images/paper.svg>';
-    random();
+    random(1);
 }
 function scissor() {
     document.getElementById('userInput').innerHTML = '<img src=images/scissor.svg>';
-    random();
+    random(2);
 }
+
 
 
 // Starts the game and calculate who is the winner //
@@ -44,3 +73,4 @@ function init() {
 
     // OPTIONAL: Calculate the scores //
 }
+
