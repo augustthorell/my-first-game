9cd1e1e0f14e5536615c25b87c56d31a9924a665
August Thorell <augustthorell@gmail.com>
2020-09-24 12:34:25 +0200

    fixed a bug

--- game2/script.js
+++ game2/script.js
@@ -2,6 +2,7 @@ const cards = document.querySelectorAll('.memory-c');
 
 function flipCard() {
    this.classList.toggle('flip');
+   console.log('test');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
